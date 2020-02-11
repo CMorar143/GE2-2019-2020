13d99f1ce2b47db47f457ed427c095d9287a9a88
CMorar143 <cian@morar.org>
2020-02-11 18:23:07 +0000

    added object and script for path

---
+++ GE2 Examples 2020/Assets/Path.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public List<Vector3> waypoints;
+    public bool isLooped = false;
+
+    private void Awake()
+    {
+        waypoints.Add(this.transform.position);
+        //waypoints.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
